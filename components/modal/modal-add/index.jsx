f46b573375c8924efe87b3d1333e0dc5442a250e
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-10-19 19:32:57 +0300

    css yapılıyo

--- components/modal/modal-add/index.jsx
+++ components/modal/modal-add/index.jsx
@@ -18,102 +18,109 @@ export default function ModalAdd({ isModalOpen }) {
   };
 
   return (
-    <dialog open className={`modalAddContainer `}>
-      <h1>New Invoice</h1>
-      <form action="">
-        <div className="billform">
-          <h3 className="formName">Bill From</h3>
-          <label htmlFor="StreetAddress">
-            <p>Street Address</p>
-            <input name="StreetAddress" type="text" placeholder="" />
-          </label>
-          <div className="billFormAddressDetail">
-            <label htmlFor="city">
-              <p> City</p>
-              <input type="text" name="city" id="" />
+    <div class="modal-overlay">
+      <dialog open className={`modalAddContainer `}>
+        <h1>New Invoice</h1>
+        <form action="">
+          <div className="billform">
+            <h3 className="formName">Bill From</h3>
+            <label htmlFor="StreetAddress">
+              <p>Street Address</p>
+              <input
+                className="streetAddress"
+                name="StreetAddress"
+                type="text"
+                placeholder=""
+              />
             </label>
-            <label htmlFor="postCode">
-              <p> Post Code</p>
-              <input type="text" name="postCode" />
-            </label>
-            <label htmlFor="country">
-              <p> Country</p>
-              <input type="text" name="country" id="" />
-            </label>
-          </div>
-        </div>
-        <div className="billToform">
-          <h3 className="formName">Bill To</h3>
-          <label htmlFor="clientName">
-            <p>Client’s Name</p>
-            <input name="clientName" type="text" placeholder="" />
-          </label>
-          <label htmlFor="clientEmail">
-            <p>Client’s Email</p>
-            <input type="email" name="clientEmail" id="" />
-          </label>
-          <label htmlFor="adres">
-            <p>Street Address</p>
-            <input type="text" name="adres" />
-          </label>
-          <div className="billToFormAddressDetail">
-            <label htmlFor="city">
-              <p> City</p>
-              <input type="text" name="city" id="" />
-            </label>
-            <label htmlFor="postCode">
-              <p> Post Code</p>
-              <input type="text" name="postCode" />
-            </label>
-            <label htmlFor="country">
-              <p> Country</p>
-              <input type="text" name="country" id="" />
-            </label>
-            <div className="date">
-              <label htmlFor="ınvoiceDate">
-                <p>Invoice Date</p>
-                <input type="date" name="ınvoiceDate" />
+            <div className="billFormAddressDetail">
+              <label htmlFor="city">
+                <p> City</p>
+                <input type="text" name="city" id="" />
               </label>
-              <div className="paymentTerms">
-                <p>Payment Terms</p>
-                <select htmlFor="paymentTerms">
-                  <option value="30">Net 30 Days</option>
-                  <option value="60">Net 60 Days</option>
-                  <option value="90">Net 90 Days</option>
-                </select>
+              <label htmlFor="postCode">
+                <p> Post Code</p>
+                <input type="text" name="postCode" />
+              </label>
+              <label htmlFor="country">
+                <p> Country</p>
+                <input type="text" name="country" id="" />
+              </label>
+            </div>
+          </div>
+          <div className="billToform">
+            <h3 className="formName">Bill To</h3>
+            <label htmlFor="clientName">
+              <p>Client’s Name</p>
+              <input name="clientName" type="text" placeholder="" />
+            </label>
+            <label htmlFor="clientEmail">
+              <p>Client’s Email</p>
+              <input type="email" name="clientEmail" id="" />
+            </label>
+            <label htmlFor="adres">
+              <p>Street Address</p>
+              <input type="text" name="adres" />
+            </label>
+            <div className="billToFormAddressDetail">
+              <label htmlFor="city">
+                <p> City</p>
+                <input type="text" name="city" id="" />
+              </label>
+              <label htmlFor="postCode">
+                <p> Post Code</p>
+                <input type="text" name="postCode" />
+              </label>
+              <label htmlFor="country">
+                <p> Country</p>
+                <input type="text" name="country" id="" />
+              </label>
+              <div className="date">
+                <label htmlFor="ınvoiceDate">
+                  <p>Invoice Date</p>
+                  <input type="date" name="ınvoiceDate" />
+                </label>
+                <div className="paymentTerms">
+                  <p>Payment Terms</p>
+                  <select htmlFor="paymentTerms">
+                    <option value="30">Net 30 Days</option>
+                    <option value="60">Net 60 Days</option>
+                    <option value="90">Net 90 Days</option>
+                  </select>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="itemList">
-          <label htmlFor="itemName">
-            <p>Item Name</p>
-            <input type="text" name="itemName" />
-          </label>
-          <label htmlFor="itemName">
-            <p>Item Name</p>
-            <input type="text" name="itemName" />
-          </label>
-        </div>
-        <button type="button" onClick={handleNewAddItem}>
-          + Add New Item
-        </button>
-        <div>
-          {addNewInput.map((input) => (
-            <>
-              <input
-                type={input.itemName.inputType}
-                placeholder={input.itemName.placeholder}
-              />
-              <input
-                type={input.quantity.inputType}
-                placeholder={input.quantity.placeholder}
-              />
-            </>
-          ))}
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </dialog>
+          <div className="itemList">
+            <label htmlFor="itemName">
+              <p>Item Name</p>
+              <input type="text" name="itemName" />
+            </label>
+            <label htmlFor="itemName">
+              <p>Item Name</p>
+              <input type="text" name="itemName" />
+            </label>
+          </div>
+          <button type="button" onClick={handleNewAddItem}>
+            + Add New Item
+          </button>
+          <div>
+            {addNewInput.map((input) => (
+              <>
+                <input
+                  type={input.itemName.inputType}
+                  placeholder={input.itemName.placeholder}
+                />
+                <input
+                  type={input.quantity.inputType}
+                  placeholder={input.quantity.placeholder}
+                />
+              </>
+            ))}
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </dialog>
+    </div>
   );
 }
