678460b6938a407fde0a73930d5b4110397216c2
Doğa Savaş <dogasvs@Doga-MacBook-Air.local>
2024-10-19 13:49:41 +0300

    svgs ve public klasörü oluşturuldu bazı svgler eklendi

---
+++ svgs/detaya-git/index.jsx
@@ -0,0 +1,7 @@
+export default function DetayaGitSvg() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="7" height="10" viewBox="0 0 7 10" fill="none">
+    <path d="M1 1L5 5L1 9" stroke="#7C5DFA" strokeWidth="2"/>
+    </svg>  
+  )
+}
