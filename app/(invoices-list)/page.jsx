5ed9210cb8f916a62a4425e58e03b998a21ff7f4
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-10-21 01:06:56 +0300

    statu de durum değişti

--- app/(invoices-list)/page.jsx
+++ app/(invoices-list)/page.jsx
@@ -2,13 +2,13 @@ import DetayaGitSvg from "@/svgs/detaya-git";
 import "./invoicesList.css";
 import Link from "next/link";
 import Header from "@/components/header";
-import { getInvoiceData } from '../actions/serverActions';
+import { getInvoiceData } from "../actions/serverActions";
 
 export default async function InvoicesList() {
   const invoices = [];
 
   try {
-    // İlk 7 faturayı çekiyoruz şimdilik 
+    // İlk 7 faturayı çekiyoruz şimdilik
     for (let i = 1; i <= 7; i++) {
       const invoice = await getInvoiceData(i);
       if (invoice) {
@@ -33,17 +33,25 @@ export default async function InvoicesList() {
                 </h3>
               </div>
               <div className="invoices-date">
-                <h3>Due {new Date(invoice.invoiceDate).toLocaleDateString()}</h3>
+                <h3>
+                  Due {new Date(invoice.invoiceDate).toLocaleDateString()}
+                </h3>
               </div>
               <div className="invoices-customname">
                 <h3>{invoice.billTo.name}</h3>
               </div>
               <div className="invoices-price">
-                <h3>£ {invoice.items.reduce((total, item) => total + item.totalPrice, 0).toFixed(2)}</h3>
+                <h3>
+                  £{" "}
+                  {invoice.items
+                    .reduce((total, item) => total + item.totalPrice, 0)
+                    .toFixed(2)}
+                </h3>
               </div>
               <div className="invoices-status">
                 <h3>
-                  <span className="doc"></span> {invoice.status === 1 ? "Paralı" : "Ödenmemiş"}
+                  <span className="doc"></span>{" "}
+                  {invoice.status === 1 ? "ödendi" : "Ödenmemiş"}
                 </h3>
               </div>
               <div className="invoices-detail">
