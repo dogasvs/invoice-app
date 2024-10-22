2ed63e3993eb9600aeb95a883b6aaf4a03f39823
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-10-22 11:00:33 +0300

    filtered dropdown yapılıyor. filtreleme işlemi için state tanımladım value verdim .

--- components/filteredDropdown/index.jsx
+++ components/filteredDropdown/index.jsx
@@ -1,20 +1,26 @@
+"use clinet";
+
+import { useState } from "react";
+
 export default function FilteredDropdown() {
+  const [selectedStatus, setSelectedStatus] = useState(null);
+
   return (
     <>
       <div className="dropdown">
         <button className="dropbtn">Duruma göre filtrele</button>
         <div class="dropdown-content">
           <label htmlFor="">
-            <input  className="cb3" type="checkbox" />
-            <p>Draft</p>
+            <input className="cb3" type="checkbox" value="draft" />
+            <p>Askıda</p>
           </label>
           <label htmlFor="">
-            <input className="cb3" type="checkbox" />
-            <p>Pending</p>
+            <input className="cb3" type="checkbox" value="pending" />
+            <p>Ödenmemiş</p>
           </label>
           <label htmlFor="">
-            <input className="cb3" type="checkbox" />
-            <p>Paid</p>
+            <input className="cb3" type="checkbox" value="paid" />
+            <p>Ödenmiş</p>
           </label>
         </div>
       </div>
