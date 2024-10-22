20a2f4dfc62e8226914bd0114ea40b4fdeabfd4b
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-10-22 16:50:57 +0300

    billto fonksiyonu hazırladım

--- components/billto/index.jsx
+++ components/billto/index.jsx
@@ -4,17 +4,43 @@ import { useState } from "react";
 
 export default function BiilTo() {
   const { filteredUSer, setFilteredUser } = useState([]);
+  const [query, setQuery] = useState("");
+
   const handleChange = (e) => {
+    const searchQuery = e.target.value;
+    setQuery(searchQuery);
+
     setTimeout(() => {
-      setFilteredUser();
+      fetchFilteredUsers(searchQuery);
     }, 500);
   };
 
-  // api / client (id)
+  const fetchFilteredUsers = async (searchQuery) => {
+    // mock datadan gelen kullanıcı verilerini filtreler
+    if (!searchQuery) {
+      setFilteredUser([]); // Arama kutusu bossa listeyi temizle
+      return;
+    }
+
+    try {
+      // api cekilcek
+      const response = await fetch("/api/users"); // api / client (id)
+      const users = await response.json();
+
+      // Kull. adlarına göre filtreleme
+      const filtered = users.filter((user) =>
+        user.name.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+
+      setFilteredUser(filtered);
+    } catch (error) {
+      console.error("Kullanıcı verisi alınırken hata oluştu:", error);
+    }
+  };
 
   return (
     <>
-      <label for="browser">Choose your browser from the list:</label>
+      <label for="browser">Listeden tarayıcınızı seçin:</label>
       <input
         list="browsers"
         onKeyUp={handleChange}
@@ -22,11 +48,15 @@ export default function BiilTo() {
         id="browser"
       />
       <datalist id="users">
-        {filteredUSer.map((user) => (
-          <option key={user.id} value={user.id}>
-            {user.name}
-          </option>
-        ))}
+        {filteredUSer ? (
+          filteredUSer.map((user) => (
+            <option key={user.id} value={user.id}>
+              {user.name}
+            </option>
+          ))
+        ) : (
+          <option value="">Kullanıcı bulunamadı</option>
+        )}
       </datalist>
     </>
   );
