c90b3c9bf49a9cc0332e4ad1ef51a41f1bf3c10d
Elif <elif.keskin233@gmail.com>
2024-10-25 18:21:28 +0300

    empty page e yönlednirme

--- components/main/index.jsx
+++ components/main/index.jsx
@@ -2,21 +2,29 @@
 import { useState, useEffect } from "react";
 import InvoicesListComponent from "../clientsInvoiceList";
 import Header from "../header";
+import { useRouter } from "next/navigation";
 
 export default function MainComponent({ invoices }) {
   const [value, setValue] = useState([]);
   const [dataInvoices, setDataInvoices] = useState(invoices);
+  const router = useRouter();
   console.log(value);
   
   useEffect(() => {
-    // Sadece sayfa yüklendiğinde çalışması için bağımlılık dizisini boş bırakıyoruz
+    // Eğer fatura sayısı 0 ise kullanıcıyı /empty sayfasına yönlendir
+    if (invoices.length === 0) {
+      router.push('/empty');
+      return;
+    }
+
+    
     const savedInvoices = localStorage.getItem("invoices");
     if (savedInvoices) {
       setDataInvoices(JSON.parse(savedInvoices));
     } else {
       localStorage.setItem("invoices", JSON.stringify(invoices));
     }
-  }, []);
+  }, [invoices, router]);
 
   return (
     <>
